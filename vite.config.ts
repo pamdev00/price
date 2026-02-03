03ee6023254feec3ef349f830c8823d347a08f04
pamparam <pamparam83@gmail.com>
2026-02-03 18:36:40 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/price/',
+  base: './',
   test: {
     globals: true,
     environment: 'happy-dom',
